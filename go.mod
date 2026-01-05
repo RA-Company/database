f995890e23b5713202408b8e5d56852112023428
Andrey Ryabov <andrey.ryabov@ra-company.kz>
2026-01-05 12:28:07 +0500

    Packages were updated.

--- go.mod
+++ go.mod
@@ -1,16 +1,16 @@
 module github.com/ra-company/database
 
-go 1.25.0
+go 1.25.5
 
 require (
-	github.com/ClickHouse/clickhouse-go/v2 v2.40.3
-	github.com/brianvoe/gofakeit/v7 v7.8.0
+	github.com/ClickHouse/clickhouse-go/v2 v2.42.0
+	github.com/brianvoe/gofakeit/v7 v7.14.0
 	github.com/georgysavva/scany/v2 v2.1.4
 	github.com/google/uuid v1.6.0
-	github.com/jackc/pgx/v5 v5.7.6
+	github.com/jackc/pgx/v5 v5.8.0
 	github.com/ra-company/env v1.0.2
-	github.com/ra-company/logging v1.0.7
-	github.com/redis/go-redis/v9 v9.14.0
+	github.com/ra-company/logging v1.0.9
+	github.com/redis/go-redis/v9 v9.17.2
 	github.com/stretchr/testify v1.11.1
 )
 
@@ -25,20 +25,20 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/klauspost/compress v1.18.2 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/paulmach/orb v0.12.0 // indirect
-	github.com/pierrec/lz4/v4 v4.1.22 // indirect
+	github.com/pierrec/lz4/v4 v4.1.23 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/segmentio/asm v1.2.1 // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
-	go.opentelemetry.io/otel v1.38.0 // indirect
-	go.opentelemetry.io/otel/trace v1.38.0 // indirect
+	go.opentelemetry.io/otel v1.39.0 // indirect
+	go.opentelemetry.io/otel/trace v1.39.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
-	golang.org/x/crypto v0.43.0 // indirect
-	golang.org/x/sync v0.17.0 // indirect
-	golang.org/x/sys v0.37.0 // indirect
-	golang.org/x/text v0.30.0 // indirect
+	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/text v0.32.0 // indirect
+	gopkg.in/Graylog2/go-gelf.v2 v2.0.0-20191017102106-1550ee647df0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- postgres/postgres_test.go
+++ postgres/postgres_test.go
@@ -1,8 +1,11 @@
 package postgres
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
+	"github.com/brianvoe/gofakeit/v7"
 	"github.com/ra-company/env"
 	"github.com/stretchr/testify/require"
 )
@@ -18,5 +21,56 @@ func Test(t *testing.T) {
 	require.NotEmpty(t, user, "PG_USER environment variable must be set for Postgress tests")
 
 	PG.Start(ctx, host, user, password, db)
-	PG.client.Close()
+
+	query := `CREATE TABLE IF NOT EXISTS test_table (
+		id SERIAL PRIMARY KEY,
+		name TEXT NOT NULL,
+		settings JSON NOT NULL);`
+	_, err := PG.Client().Exec(ctx, query)
+	require.NoError(t, err, "failed to create test_table")
+
+	defer func() {
+		_, err := PG.Client().Exec(ctx, `DROP TABLE IF EXISTS test_table;`)
+		require.NoError(t, err, "failed to drop test_table")
+		PG.Stop(ctx)
+	}()
+
+	type testStruct struct {
+		Age     int    `json:"age" db:"age"`
+		City    string `json:"city" db:"city"`
+		Country string `json:"country" db:"country"`
+	}
+
+	type testModel struct {
+		ID       int        `json:"id" db:"id"`
+		Name     string     `json:"name" db:"name"`
+		Settings testStruct `json:"settings" db:"settings"`
+	}
+
+	faker := gofakeit.New(0)
+
+	Model := testModel{}
+
+	err = faker.Struct(&Model)
+	require.NoError(t, err, "failed to generate fake data for testModel")
+
+	js, err := json.Marshal(Model.Settings)
+	require.NoError(t, err, "failed to marshal settings to JSON")
+
+	insertQuery := fmt.Sprintf(`INSERT INTO test_table (name, settings) VALUES ('%s', '%s')`, Model.Name, string(js))
+	ids, err := PG.Insert(ctx, "Model", insertQuery)
+	require.NoError(t, err, "failed to insert data into test_table")
+	require.NotZero(t, len(ids), "inserted model ID should not be zero")
+	Model.ID = int(ids[0])
+
+	var fetchedModel []testModel
+	selectQuery := fmt.Sprintf(`SELECT id, name, settings FROM test_table WHERE id = %d;`, Model.ID)
+	err = PG.Select(ctx, "Model", selectQuery, &fetchedModel)
+	require.NoError(t, err, "failed to fetch data from test_table")
+
+	require.Equal(t, Model.ID, fetchedModel[0].ID, "fetched model ID does not match inserted model ID")
+	require.Equal(t, Model.Name, fetchedModel[0].Name, "fetched model Name does not match inserted model Name")
+	require.Equal(t, Model.Settings.Age, fetchedModel[0].Settings.Age, "fetched model Settings do not match inserted model Settings")
+	require.Equal(t, Model.Settings.City, fetchedModel[0].Settings.City, "fetched model Settings do not match inserted model Settings")
+	require.Equal(t, Model.Settings.Country, fetchedModel[0].Settings.Country, "fetched model Settings do not match inserted model Settings")
 }
